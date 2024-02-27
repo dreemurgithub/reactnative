58bde280cf79a7e4faad87fae3bfe2c1aeb2b959
Dat Linux <lethanhdat1993@gmail.com>
2024-02-27 14:22:47 +0700

    1st commit

---
+++ App.js
@@ -0,0 +1,35 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View,Button, Alert } from 'react-native';
+
+export default function App() {
+  const createThreeButtonAlert = () =>
+    Alert.alert('Alert Title', 'My Alert Msg', [
+      {
+        text: 'Ask me later',
+        onPress: () => console.log('Ask me later pressed'),
+      },
+      {
+        text: 'Cancel',
+        onPress: () => console.log('Cancel Pressed'),
+        style: 'cancel',
+      },
+      {text: 'OK', onPress: () => console.log('OK Pressed')},
+    ]);
+
+  return (
+    <View style={styles.container}>
+      <Text>Hello my love, update</Text>
+      <StatusBar style="auto" />
+      <Button title='Click to alert' onPress={createThreeButtonAlert}/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
